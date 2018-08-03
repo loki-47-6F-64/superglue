1cfc2b8daab9a31e396ce1f4b4d706e8dbf6279f
loki <loki-47-6F-64@users.noreply.github.com>
2018-08-03 22:43:26 +0200

    update use of compiler to clang

--- run_djinni.sh
+++ run_djinni.sh
@@ -11,6 +11,7 @@ done
 
 # output dir should already exist
 printf "$imported" > output/all.djinni
+
 # Locate the script file.  Cross symlinks if necessary.
 loc="$0"
 while [ -h "$loc" ]; do
@@ -22,47 +23,66 @@ while [ -h "$loc" ]; do
         loc="`dirname "$loc"`/$link"  # Relative link
     fi
 done
-base_dir=$(cd `dirname "$loc"` && pwd)
+base_dir=$(cd "`dirname "$loc"`" && pwd)
 
 temp_out="$base_dir/djinni-output-temp"
 
-in="$base_dir/output/all.djinni"
+in="$base_dir/example.djinni"
 
-cpp_out="$base_dir/src/generated-src"
-objc_out="$base_dir/objc/generated-src"
-jni_out="$base_dir/jni/generated-src"
+cpp_out="$base_dir/generated-src/cpp"
+jni_out="$base_dir/generated-src/jni"
+objc_out="$base_dir/generated-src/objc"
 java_out="$base_dir/modules/superglue/src/main/java/com/loki/superglue/djinni/jni"
 
 java_package="com.loki.superglue.djinni.jni"
 
 gen_stamp="$temp_out/gen.stamp"
 
-# Build Djinni.
+if [ $# -eq 0 ]; then
+    # Normal build.
+    true
+elif [ $# -eq 1 ]; then
+    command="$1"; shift
+    if [ "$command" != "clean" ]; then
+        echo "Unexpected argument: \"$command\"." 1>&2
+        exit 1
+    fi
+    for dir in "$temp_out" "$cpp_out" "$jni_out" "$java_out"; do
+        if [ -e "$dir" ]; then
+            echo "Deleting \"$dir\"..."
+            rm -r "$dir"
+        fi
+    done
+    exit
+fi
+
+# Build djinni
 "$base_dir/djinni/src/build"
 
 [ ! -e "$temp_out" ] || rm -r "$temp_out"
-$base_dir/djinni/src/run-assume-built \
+"$base_dir/djinni/src/run-assume-built" \
     --java-out "$temp_out/java" \
     --java-package $java_package \
+    --java-nullable-annotation "javax.annotation.CheckForNull" \
+    --java-nonnull-annotation "javax.annotation.Nonnull" \
     --ident-java-field mFooBar \
     \
     --cpp-out "$temp_out/cpp" \
-    --ident-cpp-enum-type foo_bar \
-    --cpp-optional-template "util::Optional" \
-    --cpp-optional-header "<kitty/util/optional.h>" \
     --cpp-namespace "gen" \
+    --ident-cpp-enum-type foo_bar \
+    --cpp-optional-template "util::Optional"
+    --cpp-optional-header "<kitty/util/optional.h>"
     \
     --jni-out "$temp_out/jni" \
-    --ident-jni-class NativeFooBar \
-    --ident-jni-file NativeFooBar \
+    --ident-jni-class Native \
+    --ident-jni-file Native \
     \
-    --objc-out "$temp_out/objc" \
-    --objcpp-out "$temp_out/objc" \
-    --objcpp-namespace djinni_generated \
+      --objcpp-namespace djinni_generated \
+      --objc-out "$temp_out/objc" \
+      --objcpp-out "$temp_out/objc" \
     --objc-type-prefix "u" \
     \
     --idl "$in"
-
 # Copy changes from "$temp_output" to final dir.
 
 mirror() {
@@ -81,7 +101,4 @@ mirror "objc" "$temp_out/objc" "$objc_out"
 
 date > "$gen_stamp"
 
-echo "Removing temp dir..."
-rm -R "$temp_out"
-
-echo "Djinni completed."
+echo "djinni completed."
